f6bf472a7fa23843028c32710453399424c9cb26
Clodoaldo <clodoaldo.junior.crmj@gmail.com>
2023-07-18 17:28:49 -0300

    Adjusts

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+import './Banner.css'
+
+function Banner () {
+    
+    return (
+        // JSX
+
+        <header className="banner"> 
+            <img src="/imagens/banner.png" alt="O banner principal da pagina do Organo"></img>
+        </header>
+        
+    )
+}
+
+export default Banner
---
+++ src/componentes/Rodapé/Rodape.js
@@ -0,0 +1,14 @@
+import './Rodape.css'
+
+function Rodape () {
+    // JSX
+
+    return ( 
+    <header className="rodape">
+        <img src="/imagens/rodape.png" alt="Rodape principal da pagina"></img>
+    </header>
+
+    )
+}
+
+export default Rodape
